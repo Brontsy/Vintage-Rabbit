ee6f8ec35f982e6f755152d210fd0711beb3e936
Brontsy <brontsy@gmail.com>
2014-04-02 23:31:08 +1100

    Added Product Details page + Styles pages

--- Vintatge.Rabbit/Vintage.Rabbit.Web/Areas/v7/v7AreaRegistration.cs
+++ Vintatge.Rabbit/Vintage.Rabbit.Web/Areas/v7/v7AreaRegistration.cs
@@ -2,8 +2,11 @@
 
 namespace Vintage.Rabbit.Web.Areas.v7
 {
-    public class v7AreaRegistration : AreaRegistration
+    public class v7Routes : AreaRegistration
     {
+        public static string Product = "Product";
+        public static string Style = "Style";
+
         public override string AreaName
         {
             get
@@ -14,11 +17,10 @@ namespace Vintage.Rabbit.Web.Areas.v7
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
-            context.MapRoute(
-                "v7_default",
-                "v7/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
-            );
+            context.MapRoute(v7Routes.Product, url: "v7/product/{name}/{productId}", defaults: new { controller = "Product", action = "Index" });
+            context.MapRoute(v7Routes.Style, url: "v7/styles/{name}/{styleId}", defaults: new { controller = "Styles", action = "Style" });
+
+            context.MapRoute("v7_default", "v7/{controller}/{action}/{id}", new { action = "Index", id = UrlParameter.Optional });
         }
     }
 }
