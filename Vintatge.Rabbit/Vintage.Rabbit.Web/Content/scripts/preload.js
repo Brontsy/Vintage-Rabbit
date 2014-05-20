4d20985dd0d03f57980d5d6df122508f83aec76d
Brontsy <brontsy@gmail.com>
2014-05-20 20:33:00 +1000

    added journal font + hover styles for cart + login register

--- Vintatge.Rabbit/Vintage.Rabbit.Web/Content/scripts/preload.js
+++ Vintatge.Rabbit/Vintage.Rabbit.Web/Content/scripts/preload.js
@@ -1,1 +1,16 @@
 ﻿
+
+
+
+
+
+$(document).ready(function () {
+
+    $('.shopping-cart').on('mouseover', function (event) {
+
+        $(this).parents('.row').addClass('shopping-cart-hover');
+    });
+    $('.shopping-cart').on('mouseleave', function (event) {
+        $(this).parents('.row').removeClass('shopping-cart-hover');
+    });
+});
