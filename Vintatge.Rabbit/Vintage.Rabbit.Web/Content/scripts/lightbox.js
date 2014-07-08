7662525bec2d903c983e3fc1d424f660e435c6b7
Brontsy <brontsy@gmail.com>
2014-07-08 23:04:12 +1000

    added shopping cart to the preview lightbox

--- Vintatge.Rabbit/Vintage.Rabbit.Web/Content/scripts/lightbox.js
+++ Vintatge.Rabbit/Vintage.Rabbit.Web/Content/scripts/lightbox.js
@@ -8,30 +8,52 @@ $(document).ready(function () {
 
 
     $('.ajax-lightbox').featherlight(
+    {
+        targetAttr: 'href',
+        type: 'ajax',
+        variant: 'lightbox-content',
+        afterOpen: function()
         {
-            targetAttr: 'href',
-            type: 'ajax',
-            variant: 'lightbox-content',
-            afterOpen: function()
-            {
 
-                AddClickEvents();
+            AddClickEvents();
                 
-                $(window).trigger('lightbox.ajax-content-loaded');
-            }
-        });
-
-    //$('.ajax-lightbox').magnificPopup({
-    //    type: 'ajax',
-    //    closeBtnInside: true,
-    //    mainClass: 'lightbox-content',
-    //    parseAjax: function (mfpResponse) {
-    //        alert('s23');
-    //    },
-    //    ajaxContentAdded: function () {
-    //        alert('s');
-    //    }
-    //});
+            $(window).trigger('lightbox.ajax-content-loaded');
+        }
+    });
    
+    $(window).on('lightbox.ajax-content-loaded', function()
+    {
+        $('.featherlight-content .ajax-lightbox').on('click', function (e) {
+
+            e.preventDefault();
+            var url = $(this).attr('href');
+            var content = $('.featherlight-content');
+            var width = content.outerWidth();
+            var height = content.outerHeight();
+
+            content.css('min-width', width);
+            content.css('min-height', height);
+
+            $('.featherlight-inner').fadeOut(500, function () {
+
+                $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    success: function (response) {
+
+                        $('.featherlight-inner').html(response);
+                        $('.featherlight-inner').fadeIn(500);
+
+                        content.css('min-width', '0px');
+                        content.css('min-height', '0px');
+
+                        $(window).trigger('lightbox.ajax-content-loaded');
+                    }
+                });
+            });
+
+        });
+    })
 
 });
