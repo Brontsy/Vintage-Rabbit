903b150a0874f452de7e604833338f4d089e6f2d
Brontsy <brontsy@gmail.com>
2014-08-03 22:33:00 +1000

    more payment complete page updates

--- Vintatge.Rabbit/Vintage.Rabbit.Web/Models/Party/PartyViewModel.cs
+++ Vintatge.Rabbit/Vintage.Rabbit.Web/Models/Party/PartyViewModel.cs
@@ -26,6 +26,16 @@ namespace Vintage.Rabbit.Web.Models.Party
 
         public DateTime ReturnDate { get; internal set; }
 
+        public string ChildsName { get; internal set; }
+
+        public string Age { get; internal set; }
+
+        public string PartyTime { get; internal set; }
+
+        public string PartyAddress { get; internal set; }
+
+        public string RSVPDetails { get; internal set; }
+
         public PartyViewModel(Vintage.Rabbit.Parties.Entities.Party party)
         {
             this.Guid = party.Guid;
@@ -34,6 +44,11 @@ namespace Vintage.Rabbit.Web.Models.Party
             this.PartyDate = party.PartyDate;
             this.HireDate = party.HireDate;
             this.ReturnDate = party.ReturnDate;
+            this.ChildsName = party.ChildsName;
+            this.Age = party.Age;
+            this.PartyTime = party.PartyTime;
+            this.PartyAddress = party.PartyAddress;
+            this.RSVPDetails = party.RSVPDetails;
         }
     }
 }
