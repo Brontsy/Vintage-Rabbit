83d8279ba0080793ed7df938ab784ffd663f9fc9
Brontsy <brontsy@gmail.com>
2014-08-18 20:52:07 +1000

    added party hire info to  the email

--- Vintatge.Rabbit/Vintage.Rabbit.Web/Areas/Emails/Controllers/InvoiceEmailController.cs
+++ Vintatge.Rabbit/Vintage.Rabbit.Web/Areas/Emails/Controllers/InvoiceEmailController.cs
@@ -5,11 +5,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vintage.Rabbit.Interfaces.CQRS;
+using Vintage.Rabbit.Membership.Entities;
+using Vintage.Rabbit.Membership.QueryHandlers;
 using Vintage.Rabbit.Orders.Entities;
 using Vintage.Rabbit.Orders.QueryHandlers;
+using Vintage.Rabbit.Parties.Entities;
+using Vintage.Rabbit.Parties.QueryHandlers;
 using Vintage.Rabbit.Products.Entities;
 using Vintage.Rabbit.Products.QueryHandlers;
 using Vintage.Rabbit.Web.Areas.Emails.Models.InvoiceEmail;
+using Vintage.Rabbit.Web.Models.Payment;
 
 namespace Vintage.Rabbit.Web.Areas.Emails.Controllers
 {
@@ -37,6 +42,36 @@ namespace Vintage.Rabbit.Web.Areas.Emails.Controllers
             ViewBag.WebsiteUrl = this._websiteUrl;
 
             return View("InvoiceEmail", viewModel);
+        }
+
+        public ActionResult PartyHireInformation(Guid orderGuid)
+        {
+            Order order = this._queryDispatcher.Dispatch<Order, GetOrderQuery>(new GetOrderQuery(orderGuid));
+
+            if (order.ContainsHireProducts() || order.ContainsTheme())
+            {
+                Party party = this._queryDispatcher.Dispatch<Party, GetPartyByOrderGuidQuery>(new GetPartyByOrderGuidQuery(order.Guid));
+                if (party != null)
+                {
+                    PartyViewModel viewModel = new PartyViewModel(party);
+
+                    if (party.DropoffAddress.HasValue)
+                    {
+                        var address = this._queryDispatcher.Dispatch<Address, GetAddressByGuidQuery>(new GetAddressByGuidQuery(party.DropoffAddress.Value));
+                        viewModel.DropoffAddress = new DeliveryAddressViewModel(address);
+                    }
+
+                    if (party.PickupAddress.HasValue)
+                    {
+                        var address = this._queryDispatcher.Dispatch<Address, GetAddressByGuidQuery>(new GetAddressByGuidQuery(party.PickupAddress.Value));
+                        viewModel.PickupAddress = new DeliveryAddressViewModel(address);
+                    }
+
+                    return this.PartialView("PartyHireInformation", viewModel);
+                }
+            }
+
+            return null;
         }
 	}
 }
---
+++ Vintatge.Rabbit/Vintage.Rabbit.Web/Areas/Emails/Models/InvoiceEmail/PartyViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vintage.Rabbit.Parties.Enums;
+using Vintage.Rabbit.Web.Models.Payment;
+
+namespace Vintage.Rabbit.Web.Areas.Emails.Models.InvoiceEmail
+{
+    public class PartyViewModel
+    {
+
+        public Guid Guid { get; internal set; }
+
+        public Guid OrderGuid { get; internal set; }
+
+        public PartyStatus Status { get; internal set; }
+
+        public DateTime PartyDate { get; internal set; }
+
+        public DeliveryAddressViewModel DropoffAddress { get; internal set; }
+
+        public DeliveryAddressViewModel PickupAddress { get; internal set; }
+
+        public DateTime HireDate { get; internal set; }
+
+        public DateTime ReturnDate { get; internal set; }
+
+        public string ChildsName { get; internal set; }
+
+        public string Age { get; internal set; }
+
+        public string PartyTime { get; internal set; }
+
+        public string PartyAddress { get; internal set; }
+
+        public string RSVPDetails { get; internal set; }
+
+        public PartyViewModel(Vintage.Rabbit.Parties.Entities.Party party)
+        {
+            this.Guid = party.Guid;
+            this.OrderGuid = party.OrderGuid;
+            this.Status = party.Status;
+            this.PartyDate = party.PartyDate;
+            this.HireDate = party.HireDate;
+            this.ReturnDate = party.ReturnDate;
+            this.ChildsName = party.ChildsName;
+            this.Age = party.Age;
+            this.PartyTime = party.PartyTime;
+            this.PartyAddress = party.PartyAddress;
+            this.RSVPDetails = party.RSVPDetails;
+        }
+    }
+}
